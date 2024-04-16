47e36fa4ffef035a1ea101955ed8322ce1337318
Summer Liu <summer.liu@myviewboard.com>
2024-04-16 18:23:06 +0800

    install and config daisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,15 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['light'],
+        },
+      },
+    ],
+  },
   theme: {
     extend: {
       backgroundImage: {
@@ -16,6 +25,7 @@ const config: Config = {
     },
   },
   plugins: [
+    require('daisyui'),
     require('tailwindcss-rtl'),
     require('tailwindcss-dir')(),
   ],
