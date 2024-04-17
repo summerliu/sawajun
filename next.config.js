e9a97f2ae968521b42c54e425e6a0f4098260772
Summer Liu <summer.liu@myviewboard.com>
2024-04-18 07:28:04 +0800

    add ts checker

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 const { i18n } = require('./next-i18next.config');
+const webpack = require('webpack');
 
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  webpack: (config, options) => {
+    const { isServer } = options;
+    // Do not run type checking twice:
+    if (isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
